d0262a491c1ef01c255d06452aecf6bbac230a67
Diego <diegoluizcsti@gmail.com>
2018-02-18 15:38:12 -0300

    resolvido problema no devolver menssagem

--- exercicios/evolverArquivo.js
+++ exercicios/evolverArquivo.js
@@ -5,14 +5,13 @@ const bot = new Telegraf(env.token)
 
 bot.on('voice', async ctx => {
     const id = ctx.update.message.voice.file_id
-    const res = await axios.get(`${env.apiUrl}/getFile?file_id=${id}`)
-    ctx.replyWithVoice({ url:`${env.apiFileUrl}/${res.data.result.file_path}`})
-})
-
-bot.on('photo', async ctx => {
+    const res = await axios.get(`${env.apiURL}/getFile?file_id=${id}`)
+    ctx.replyWithVoice({url: `${env.apiFileUrl}/${res.data.result.file_path}`})
+   })
+   bot.on('photo', async ctx => {
     const id = ctx.update.message.photo[0].file_id
-    const res = await axios.get(`${env.apiUrl}/getFile?file_id=${id}`)
-    ctx.replyWithPhoto({ url: `${env.apiFileUrl}/${res.data.result.file_path}`})
-})
+    const res = await axios.get(`${env.apiURL}/getFile?file_id=${id}`)
+    ctx.replyWithPhoto({url: `${env.apiFileUrl}/${res.data.result.file_path}`})
+   })
 
 bot.startPolling()
---
+++ projetos/listaCompras/listaCompras.js
@@ -0,0 +1,6 @@
+const env = require('../../.env')
+const Telegraf = require('telegraf')
+const Extra = require('telegraf/extra')
+const Markup = require('telegraf/markup')
+const bot = new Telegraf(env.token)
+
